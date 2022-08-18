4e4bf2349304f73fd5a5914e4ee53a8bc8fc745e
Gaxihyus <gaxyhs@hotmail.com>
2022-08-18 17:41:04 -0300

    test

--- Program.cs
+++ Program.cs
@@ -4,6 +4,16 @@ namespace CalculadoraEnem
 {
     class Program
     {
+        // Pesos
+        float pesoLing;
+        float pesoHum;
+        float pesoNat;
+        float pesoMat;
+
+        // Notas
+        double notaMat;        
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
